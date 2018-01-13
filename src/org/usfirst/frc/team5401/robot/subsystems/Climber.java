e346bc1872f46e133ff998b33a6c240aa3780b57
jxfireruby <jxfireruby@gmail.com>
2018-01-13 15:46:27 -0500

    Started working on the Climber all the needed files are imported

--- src/org/usfirst/frc/team5401/robot/subsystems/Climber.java
+++ src/org/usfirst/frc/team5401/robot/subsystems/Climber.java
@@ -1,11 +1,22 @@
 package org.usfirst.frc.team5401.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Solenoid;
+
+import org.usfirst.frc.team5401.robot.RobotMap;
+
+
 
 /**
  *
  */
 public class Climber extends Subsystem {
+	
 	public Climber(){
 	
 	}
