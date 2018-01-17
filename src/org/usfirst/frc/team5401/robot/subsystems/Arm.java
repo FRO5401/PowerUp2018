e6b167aedded13feb1f1ff1690e235a8b3e2bc5c
jxfireruby <jxfireruby@gmail.com>
2018-01-16 20:31:02 -0500

    Revert "Revert "Set for PID Created. Methods are still required""
    This reverts commit a612658fe5e304bfd96c428ffeda197bd08f2e44.

--- src/org/usfirst/frc/team5401/robot/subsystems/Arm.java
+++ src/org/usfirst/frc/team5401/robot/subsystems/Arm.java
@@ -21,37 +21,55 @@ public class Arm extends Subsystem {
 	private Solenoid brake;
 	private Encoder armEncoder;
 	private TalonSRX armTalon;
-	private AnalogPotentiometer armPot;
-	private VictorSP armMotor;
+	private AnalogPotentiometer armPot;//Probably will not be used
 	
-	private double motorSpeed;
-	private double kP, kI, kD;
+	
 	private double armAngle;
 	
 	private boolean armPidEnabled;
 	
-	private int THRESH;
+	private int loopIndex;
 	
 	public Arm(){
 		
-		//TalonSRX
-		armTalon = new TalonSRX(0);
-		armTalon.set(ControlMode.Velocity, 0);
+		loopIndex = 0;
 		
-		armTalon.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 10);
-		THRESH = 0;
+		//Object instantiation
+		armTalon = new TalonSRX(0);//TODO need to check on RoboRIO
+		armPot = new AnalogPotentiometer(RobotMap.ARM_POT_CHANNEL, RobotMap.ARM_RANGE, RobotMap.ARM_OFFSET);
 		
+		/******REPEAT THE FOLLOWING LINE TO MAKE SET POINTS*********/
+		//0 is the output value, possibly position wanted? in encoder tick or analog value, not sure native value
+		//Sets up the Control mode to PID position
+		armTalon.set(ControlMode.Position, 0);		
+		
+		//Sets up feedback device for PID
+		armTalon.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, loopIndex, RobotMap.TIMEOUT_LIMIT_IN_Ms);//10 is a timeout that waits for successful conection to sensor
+		armTalon.setSensorPhase(true);//true if sensor value is positive if the motor controller output is negative. False if both are positive or negative
+		
+		//Sets allowable error, which is how far the actual value is off from intended value
+		//0 is slot index, which is parameter slot for the constant. Not sure what this actually does
+		armTalon.configAllowableClosedloopError(loopIndex, RobotMap.ARM_THRESHOLD_FOR_PID, RobotMap.TIMEOUT_LIMIT_IN_Ms);
+		
+		//Sets max and min reverse and forward. First number is max/min output in percent 1 = 100%
+        armTalon.configNominalOutputForward(0, 	RobotMap.TIMEOUT_LIMIT_IN_Ms);
+        armTalon.configNominalOutputReverse(0, 	RobotMap.TIMEOUT_LIMIT_IN_Ms);
+        armTalon.configPeakOutputForward(1, 	RobotMap.TIMEOUT_LIMIT_IN_Ms);
+        armTalon.configPeakOutputReverse(-1, 	RobotMap.TIMEOUT_LIMIT_IN_Ms);
+        
+        //Sets F, P, I, and D
+        //Middle parameter is the corresponding value for F, P, I, or D
+        armTalon.config_kF(loopIndex, RobotMap.ARM_kF, RobotMap.TIMEOUT_LIMIT_IN_Ms);
+        armTalon.config_kP(loopIndex, RobotMap.ARM_kP, RobotMap.TIMEOUT_LIMIT_IN_Ms);
+        armTalon.config_kI(loopIndex, RobotMap.ARM_kI, RobotMap.TIMEOUT_LIMIT_IN_Ms);
+        armTalon.config_kD(loopIndex, RobotMap.ARM_kD, RobotMap.TIMEOUT_LIMIT_IN_Ms);        
+        
+        
 		//PID Stuff
 		armPidEnabled = true;
-		   /*SmartDashboard.putBoolean("PID_Enabled", pidEnabled);
-		   SmartDashboard.putNumber("feed_forward", feed_forward);
-		   SmartDashboard.putNumber("kP", kP);
-		   SmartDashboard.putNumber("kI", kI);
-		   SmartDashboard.putNumber("kD", kD);
-		   */
 		
-		//armPot
-		armPot = new AnalogPotentiometer(RobotMap.armPotChannel, RobotMap.armRange, RobotMap.armOffest);
+
+		
 		
 		
 	}
