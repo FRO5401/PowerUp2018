0cddf8a27a9876262f8f7d62645db3d524363c75
jxfireruby <jxfireruby@gmail.com>
2018-02-02 20:13:32 -0500

    Changed Distances

--- src/org/usfirst/frc/team5401/robot/autonomous/AutoCenterSwitch.java
+++ src/org/usfirst/frc/team5401/robot/autonomous/AutoCenterSwitch.java
@@ -12,26 +12,25 @@ public class AutoCenterSwitch extends CommandGroup {
     	String gameData = DriverStation.getInstance().getGameSpecificMessage();
     	if(gameData.charAt(0) == 'L')
     	{
-    	//Start at Auto Position #3. Drive Forward 55 inches
+    	//Start at Auto Position #3. 
     	//This will be putting block on left side (Closer to Left)
     		addSequential(new AutoDrive(55, 1));
     		addSequential(new AutoTurnAngle(-90));
-    		addSequential(new AutoDrive(40, 1));
+    		addSequential(new AutoDrive(30, 1));
     		addSequential(new AutoTurnAngle(90));
     		addSequential(new AutoDrive(45, 1));
     		//addSequential(new xxxx());
     	}
     	else 
     	{    	
-    	//Start at Auto Position #3. Drive Forward 55 inches
+    	//Start at Auto Position #3. 
     	/*
     		//This will be putting block the right side
-    		addSequential(new AutoDrive(70, 1));
+    		addSequential(new AutoDrive(55, 1));
     		addSequential(new AutoTurnAngle(90));
-    		addSequential(new AutoDrive(56, 1));
+    		addSequential(new AutoDrive(45, 1));
     		addSequential(new AutoTurnAngle(-90));
-    		addSequential(new AutoDrive(70, 1));
-    		//addSequential(new xxxx());
+    		addSequential(new AutoDrive(45, 1));
     		 * 
     		 */
     	}
