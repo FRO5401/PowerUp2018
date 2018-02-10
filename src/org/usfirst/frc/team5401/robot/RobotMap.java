5cbc54beed37dc366233a00d8449b359237de4ce
NyarlathoTech <damienolsen2001@gmail.com>
2018-02-09 20:56:30 -0500

    Added left bumper setpoint

--- src/org/usfirst/frc/team5401/robot/RobotMap.java
+++ src/org/usfirst/frc/team5401/robot/RobotMap.java
@@ -118,5 +118,6 @@ public class RobotMap {
 	public static final int FLOOR_SETPOINT = 0;
 	public static final int PORTAL_SETPOINT = 0;
 	public static final int SET_SWITCH_SETPOINT = 0;
+	public static final int SCALE_SETPOINT = 0;
 	public static final int HIGHEST_RUNG_SETPOINT = 0;
 }
