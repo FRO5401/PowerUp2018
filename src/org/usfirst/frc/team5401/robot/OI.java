375180859ff87b6ad42c7f3d519216c2f8bdabf8
ptoil <suhailmallick@gmail.com>
2018-02-11 10:53:54 -0500

    Added Button Objects

--- src/org/usfirst/frc/team5401/robot/OI.java
+++ src/org/usfirst/frc/team5401/robot/OI.java
@@ -12,6 +12,33 @@ import org.usfirst.frc.team5401.robot.commands.*;
  */
 public class OI {
 	
+	//User Maps
+	Joystick xboxController_Driver = new Joystick(RobotMap.XBOX_CONTROLLER_DRIVER);
+	Joystick xboxController_Operator = new Joystick(RobotMap.XBOX_CONTROLLER_OPERATOR);
+	
+	//Buttons
+	Button xboxA_Driver			  = new JoystickButton(xboxController_Driver, 1);
+	Button xboxB_Driver			  = new JoystickButton(xboxController_Driver, 2);
+	Button xboxX_Driver			  = new JoystickButton(xboxController_Driver, 3);
+	Button xboxY_Driver			  = new JoystickButton(xboxController_Driver, 4);
+	Button xboxLeftBumper_Driver  = new JoystickButton(xboxController_Driver, 5);
+	Button xboxRightBumper_Driver = new JoystickButton(xboxController_Driver, 6);
+	Button xboxBack_Driver		  = new JoystickButton(xboxController_Driver, 7);
+	Button xboxStart_Driver		  = new JoystickButton(xboxController_Driver, 8);
+	Button xboxL3_Driver		  = new JoystickButton(xboxController_Driver, 9);
+	Button xboxR3_Driver		  = new JoystickButton(xboxController_Driver, 10);
+	
+	Button xboxA_Operator			= new JoystickButton(xboxController_Operator, 1);
+	Button xboxB_Operator			= new JoystickButton(xboxController_Operator, 2);
+	Button xboxX_Operator			= new JoystickButton(xboxController_Operator, 3);
+	Button xboxY_Operator			= new JoystickButton(xboxController_Operator, 4);
+	Button xboxLeftBumper_Operator  = new JoystickButton(xboxController_Operator, 5);
+	Button xboxRightBumper_Operator = new JoystickButton(xboxController_Operator, 6);
+	Button xboxBack_Operator		= new JoystickButton(xboxController_Operator, 7);
+	Button xboxStart_Operator		= new JoystickButton(xboxController_Operator, 8);
+	Button xboxL3_Operator		  	= new JoystickButton(xboxController_Operator, 9);
+	Button xboxR3_Operator		  	= new JoystickButton(xboxController_Operator, 10);
+	
 	//Assign values from RobotMap here
 	int leftStickX = RobotMap.XBOX_AXIS_LEFT_X;
 	int leftStickY = RobotMap.XBOX_AXIS_LEFT_Y;
@@ -38,10 +65,6 @@ public class OI {
 		
 	}
 	
-	//User Maps
-	Joystick xboxController_Driver = new Joystick(RobotMap.XBOX_CONTROLLER_DRIVER);
-	Joystick xboxController_Operator = new Joystick(RobotMap.XBOX_CONTROLLER_OPERATOR);
-
 	//Controller Axis
 	public double xboxAxis(int input, Joystick userMap) {
 		return userMap.getRawAxis(input);
