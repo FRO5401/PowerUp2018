86df8057d39852b48e34c52232142c9abf074ec6
ptoil <suhailmallick@gmail.com>
2018-02-17 11:25:24 -0500

    Current RobotMap is up to date

--- src/org/usfirst/frc/team5401/robot/RobotMap.java
+++ src/org/usfirst/frc/team5401/robot/RobotMap.java
@@ -107,6 +107,11 @@ public class RobotMap {
 	public static final int WRIST_MOVE_LONG_BACKWARD  = 3;
 	public static final int WRIST_MOVE_SHORT_FORWARD  = 5;
 	public static final int WRIST_MOVE_SHORT_BACKWARD = 6;
+	public static final int ROLLER_CLAW_SHORT_OUT		= 2;
+	public static final int ROLLER_CLAW_SHORT_IN		= 4;
+	public static final int ROLLER_CLAW_LONG_OUT		= 0;
+	public static final int ROLLER_CLAW_LONG_IN			= 1;
+	
 	
 	//DIO Sensors
 	public static final int DRIVE_ENC_LEFT_A 	= 0;
