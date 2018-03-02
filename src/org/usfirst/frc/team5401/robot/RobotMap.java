140bec80a0e024b7608421768d3dfeb202ae8d66
ptoil <suhailmallick@gmail.com>
2018-03-02 18:44:53 -0500

    RobotMap matches RobotMap SpreadSheet

--- src/org/usfirst/frc/team5401/robot/RobotMap.java
+++ src/org/usfirst/frc/team5401/robot/RobotMap.java
@@ -136,9 +136,9 @@ public class RobotMap {
 	public static final int WRIST_MOVE_SHORT_FORWARD  = 5;
 	public static final int WRIST_MOVE_SHORT_BACKWARD = 6;
 
-	public static final int ARM_BRAKE = 0;
+	public static final int ARM_BRAKE = 1;
 	public static final int ROLLER_CLAW_SHORT_OUT		= 2;
-	public static final int ROLLER_CLAW_SHORT_IN		= 1;
+	public static final int ROLLER_CLAW_SHORT_IN		= 4;
 	public static final int ROLLER_CLAW_LONG_OUT		= 0;//Currently 0 because physically not allocated and commented out in code
 	public static final int ROLLER_CLAW_LONG_IN			= 0;//Currently 0 because physically not allocated and commented out in code
 
