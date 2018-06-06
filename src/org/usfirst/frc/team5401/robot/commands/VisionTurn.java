0bbc50f81a969f6bcd789b25ead528523b109be6
jxfireruby <jxfireruby@gmail.com>
2018-06-06 18:40:50 -0400

    Revert "Vision Code"
    This reverts commit 2de2b0183572aed6875899a29c8ee3a1649f1b93.

--- src/org/usfirst/frc/team5401/robot/commands/VisionTurn.java
+++ src/org/usfirst/frc/team5401/robot/commands/VisionTurn.java
@@ -1,64 +1,46 @@
 package org.usfirst.frc.team5401.robot.commands;
 
-import org.usfirst.frc.team5401.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import org.usfirst.frc.team5401.robot.Robot;
+import org.usfirst.frc.team5401.robot.autonomous.*;
 /**
  *
  */
 public class VisionTurn extends Command {
 
-	private double desiredTurnAngleRelativeToInitAnglePID;
-	private double currentAngleRelativeToInitAngle;
-	private boolean doneTurn;
+	private double desiredTurnAngleRelativeToInitAngle;
 	
     public VisionTurn() {
         // Use requires() here to declare subsystem dependencies
+    	requires(Robot.drivebase);
         requires(Robot.dumbcamera);
-        requires(Robot.drivebase);
-        
-    	desiredTurnAngleRelativeToInitAnglePID = Robot.dumbcamera.visionLoopSynchronized();
-    	currentAngleRelativeToInitAngle = 0;
-    	doneTurn = false;
+        desiredTurnAngleRelativeToInitAngle = Robot.dumbcamera.visionLoopSynchronized();
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	Robot.drivebase.gyroReset();
-    	Robot.drivebase.enableTurnPID();
-    	Robot.drivebase.setTurnSetpoint(desiredTurnAngleRelativeToInitAnglePID);
-        doneTurn = false;
+    	Scheduler.getInstance().add(new AutoPIDTurnAngle(desiredTurnAngleRelativeToInitAngle));
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	currentAngleRelativeToInitAngle = Robot.drivebase.getGyroAngle();
-     	SmartDashboard.putNumber("Relative to Inital Angle", currentAngleRelativeToInitAngle);
-      	if(Robot.drivebase.getTurnPIDOnTarget())
-     	{
-      		Robot.drivebase.disableTurnPID();
- 			Robot.drivebase.stop();
- 			doneTurn = true;
-     	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return doneTurn;
+    	return true;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-    	//Something to note: if the vision processing does not find anything, the coordinates 0,0 will be returned.
-    	//As a result the robot should turn one direction to the end of the camera vision. 
-    	//NEED to add a conditional if that movement is done, repeat the action.
-    	Robot.drivebase.disableTurnPID();
+    	
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	Robot.drivebase.disableTurnPID();
     }
 }
