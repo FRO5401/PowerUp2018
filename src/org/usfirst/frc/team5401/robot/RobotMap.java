1bd4daaf477661657ff821342765aca7273edd5a
ptoil <suhailmallick@gmail.com>
2018-06-06 17:34:29 -0400

    Henry Robot Map changed for PowerUp code

--- src/org/usfirst/frc/team5401/robot/RobotMap.java
+++ src/org/usfirst/frc/team5401/robot/RobotMap.java
@@ -126,14 +126,14 @@ public class RobotMap {
 	 
 	//Motors
 		//Drive Motors
-	public static final int DRIVE_RIGHT_MOTOR_1 = 0;
-	public static final int DRIVE_RIGHT_MOTOR_2 = 1;
-	public static final int DRIVE_LEFT_MOTOR_1 	= 2;
-	public static final int DRIVE_LEFT_MOTOR_2 	= 3;	
+	public static final int DRIVE_RIGHT_MOTOR_1 = 1;
+	public static final int DRIVE_RIGHT_MOTOR_2 = 8;
+	public static final int DRIVE_LEFT_MOTOR_1 	= 0;
+	public static final int DRIVE_LEFT_MOTOR_2 	= 7;	
 
 		//Climber Motor
 	public static final int CLIMBER_MOTOR_TOP = 6;
-	public static final int CLIMBER_MOTOR_BOTTOM = 7;
+	public static final int CLIMBER_MOTOR_BOTTOM = 3;
 
 
 		//Infeed Motors
